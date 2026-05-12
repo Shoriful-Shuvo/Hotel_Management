d561939803adfb90c980140aebfa49a1be06e5b7
rahid5650 <rahidul5650@gmail.com>
2026-05-13 01:08:35 +0600

    Creating Guest class

---
+++ src/Guest.java
@@ -0,0 +1,10 @@
+abstract class Guest {
+    private String GuestName;
+    private int GuestRoom;
+    private int PhoneNumber;
+
+    public Guest(int ID,String Name,int PhoneNumber){
+
+    }
+    public abstract void rest();
+}
