cd62281c0823972c1519b9f7580ca5494a2f2f1f
rahid5650 <rahidul5650@gmail.com>
2026-05-13 01:11:32 +0600

    Business class Created

---
+++ src/Business.java
@@ -0,0 +1,10 @@
+public class Business extends Guest{
+    Business(int ID,String Name,int PhoneNumber){
+        super(ID,Name,PhoneNumber);
+    }
+
+    @Override
+    public void rest(){
+
+    }
+}
