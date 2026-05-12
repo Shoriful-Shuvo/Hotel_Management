ba999cb771bdab15be705f6ea4b4ea01ed67e40c
Rifatul Islam Shakib <2024100000124@seu.edu.bd>
2026-05-12 21:54:26 +0600

    Abstract method declare

--- src/HotelMangement.java
+++ src/HotelMangement.java
@@ -1,3 +1,16 @@
-public class HotelMangement {
+public abstract class HotelMangement {
+    private String ID;
+    private String Name;
+    private String PhoneNumber;
+
+    public HotelMangement(String ID, String Name, String PhoneNumber){
+        this.ID=ID;
+        this.Name=Name;
+        this.PhoneNumber=PhoneNumber;
+    }
+
+    public abstract String getSummary();
+
+
 
 }
