dc56b5e70fa6b3c1a5f3d2406345b6422bf3dd04
Rifatul Islam Shakib <2024100000124@seu.edu.bd>
2026-05-12 21:22:13 +0600

    Members Declaration

--- src/Reservation.java
+++ src/Reservation.java
@@ -1,5 +1,10 @@
 import java.time.LocalDate;
 public class Reservation {
-
+    private LocalDate arrivalDate;
+    private LocalDate departureDate;
+    private Guest guest;
+    private Room room;
+    private double price;
+    private String status;
 
 }
