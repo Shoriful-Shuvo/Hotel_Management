cd63d36d1e74492bc17c50f21d61ed19b3bd3c1c
Istiak_Ahamed <istiakop800@gmail.com>
2026-05-12 20:33:02 +0600

    Adding Constructor

--- src/Room.java
+++ src/Room.java
@@ -1,3 +1,12 @@
 public abstract class Room {
+    private int RoomNumber;
+    private int Floor;
+    private double Price;
+
+    public Room (int RoomNumber,int Floor,double Price) {
+         this.RoomNumber =RoomNumber;
+         this.Floor = Floor;
+         this.Price = Price;
+    }
 
 }
