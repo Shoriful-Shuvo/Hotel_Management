be7157ba9b57d348cd1f9c65bbb20296d8c9b419
Mr-Abdul-Ahad <2024100000140@seu.edu.bd>
2026-05-13 01:28:49 +0600

    Creating mingpool

---
+++ src/SwimmingPool.java
@@ -0,0 +1,7 @@
+public class SwimmingPool extends Facility {
+    public SwimmingPool(){}
+    @Override
+    public void Facility() {
+        System.out.println("Facility Type: Swimming Poool");
+    }
+}
