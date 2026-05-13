89f0ba7149cc99efc8749c0506c3076b4f19ad7b
Istiak_Ahamed <istiakop800@gmail.com>
2026-05-13 07:59:30 +0600

    Luxury Room facilities

--- src/LuxuryRoom.java
+++ src/LuxuryRoom.java
@@ -1,3 +1,11 @@
 public class LuxuryRoom extends Room {
+    private String FoolService;
+
+    public LuxuryRoom() {}
+
+    public LuxuryRoom(String RoomName,int RoomNumber,int Floor,double Price,boolean AvailableRoom,String FoodService) {
+        super(RoomName,RoomNumber,Floor,Price,AvailableRoom);
+        this.FoolService = FoodService;
+    }
 
 }
