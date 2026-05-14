60d07267c71b4afff2b06cdab483c465ffcdf42a
Istiak_Ahamed <istiakop800@gmail.com>
2026-05-15 01:16:15 +0600

    LuxuryRoom

--- src/LuxuryRoom.java
+++ src/LuxuryRoom.java
@@ -1,11 +1,14 @@
 public class LuxuryRoom extends Room {
-    private String FoolService;
+    private boolean hasFridge;
+    private boolean hasSmartTv;
+    private String bedType;
 
-    public LuxuryRoom() {}
-
-    public LuxuryRoom(String RoomNumber,int floorLevel,double costPerNight,String FoodService) {
+    public LuxuryRoom(String RoomNumber,int floorLevel,double costPerNight,boolean hasFridge,boolean hasSmartTv,String bedType) {
         super(RoomNumber,floorLevel,costPerNight,true);
-        this.FoolService = FoodService;
+        this.hasFridge = hasFridge;
+        this.hasSmartTv = hasSmartTv;
+        this.bedType = bedType;
+
     }
 
 
